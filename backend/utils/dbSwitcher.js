57f5d1ccba092dd9ca7b82e89408043a2e672a5c
werewolfcoder <157696808+werewolfcoder@users.noreply.github.com>
2025-04-08 11:01:03 +0530

    Update dbSwitcher.js

--- backend/utils/dbSwitcher.js
+++ backend/utils/dbSwitcher.js
@@ -1,20 +1,31 @@
 require('dotenv').config(); // Load .env
+// const mongoose = require("mongoose");
+// const connections = {};
+// function getHotelDb(hotelName) {
+//   if (connections[hotelName]) {return connections[hotelName];}
+//   const baseUri = process.env.BASE_MONGO_URI;
+//   const dbName = `orderingSystem_${hotelName}`;
+//   const fullUri = `${baseUri}${dbName}`;
+//   connections[hotelName] = mongoose.createConnection(fullUri);
+//  return connections[hotelName];}
+// module.exports = { getHotelDb };
 const mongoose = require("mongoose");
 
-const connections = {};
+const connections = {}; // cache to avoid reconnecting
 
 function getHotelDb(hotelName) {
   if (connections[hotelName]) {
     return connections[hotelName];
   }
 
-  const baseUri = process.env.BASE_MONGO_URI;
-  const dbName = `orderingSystem_${hotelName}`;
-  const fullUri = `${baseUri}${dbName}`;
+  const uri = `${process.env.BASE_MONGO_URI}/${hotelName}?retryWrites=true&w=majority&appName=orderingSystem`;
+  const conn = mongoose.createConnection(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-  connections[hotelName] = mongoose.createConnection(fullUri);
-
-  return connections[hotelName];
+  connections[hotelName] = conn;
+  return conn;
 }
 
-module.exports = { getHotelDb };
+module.exports = getHotelDb;
